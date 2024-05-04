5615f7392fb80c3d5ff6605eb14b4cfea9854c16
MuhammadDeen-1 <143871772+MuhammadDeen-1@users.noreply.github.com>
2024-05-05 00:55:05 +1000

    commit

---
+++ etl.py
@@ -0,0 +1,13 @@
+import os, uuid
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from dotenv import load_dotenv 
+import pandas as pd 
+
+account_url = "https://newtestdatabase.blob.core.windows.net"
+default_credential = DefaultAzureCredential()
+blob_service_client = BlobServiceClient(account_url, credential=default_credential) 
+local_path = "./data"
+
+
+    
--- main.py
+++ main.py
@@ -2,11 +2,51 @@ import os, uuid
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 from dotenv import load_dotenv 
-from utils.datasetup import *
 import pandas as pd 
-from utils.dimensions_classes import *
 
-load_dotenv() 
+try:
+    print("Azure Blob Storage Python quickstart sample")
 
-model = ModelAbstract()
-model.dimension_generator("Model", columns=['Title', 'Brand', 'Year'])
+    account_url = "https://newtestdatabase.blob.core.windows.net"
+    default_credential = DefaultAzureCredential()
+
+    # Create the BlobServiceClient object
+    blob_service_client = BlobServiceClient(account_url, credential=default_credential) 
+    
+        # Create a local directory to hold blob data
+    local_path = "./data"
+   
+    # Create a unique name for the container
+    #container_name = "car-dataset"
+
+    # Create the container
+    #container_client = blob_service_client.create_container(container_name)
+
+
+    ################## Create a file in the local data directory to upload and download #################
+    #local_file_name = str("hello Blob") + ".txt"
+    #upload_file_path = os.path.join(local_path, local_file_name)
+    
+    
+    # Write text to the file
+    #    file = open(file=upload_file_path, mode='w')
+    #    file.write("Hello, World!")
+    #    file.close()
+
+    
+    ############ Create a blob client using the local file name as the name for the blob #################
+    ##### blob_client = blob_service_client.get_blob_client(container='car-dataset', blob=local_file_name)
+
+    #print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
+
+    ################## Upload the created file #################
+   # with open(file=upload_file_path, mode="rb") as data:
+   # blob_client.upload_blob(data)   
+        
+ 
+
+except Exception as ex:
+    print('Exception:')
+    print(ex)
+
+
